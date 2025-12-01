33119bf7f63cb632e32e21cc20e05c1984286050
amitlals <amlal@microsoft.com>
2025-12-01 00:08:59 -0600

    Disable API schema generation to work around Gradio 4.44.1 JSON schema bug

--- app_gradio.py
+++ app_gradio.py
@@ -1459,5 +1459,6 @@ if __name__ == "__main__":
         server_name=server_name,
         server_port=server_port,
         share=False,
-        show_error=True
+        show_error=True,
+        show_api=False
     )
